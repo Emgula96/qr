3eb2e8c00a1b8220de28826e1e894efbda499456
Bryan Wheeler <bwheel181@gmail.com>
2023-11-13 07:58:18 -0600

    Adds common page footer and styling

--- shared/src/components/Page/Page.jsx
+++ shared/src/components/Page/Page.jsx
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types'
 
 function Page({ children }) {
     return (
-        <div className='page-wrapper'>
-            <div className='page-banner'>
-                <img src="/accent.png"></img>
+        <>
+            <div className='page-wrapper'>
+                <div className='page-banner'>
+                    <img src="/accent.png" />
+                </div>
+                <div className='page-content'>
+                    {children}
+                </div>
+                <div className='page-footer'>
+                <img src="/footer.png" />
+                </div>
             </div>
-            {children}
-        </div>
+        </>
       )
 }
 
