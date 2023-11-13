4a184c052ae63c9310dbdd61bd1959d1d1f43d8d
Bryan Wheeler <bwheel181@gmail.com>
2023-11-13 14:46:20 -0600

    Add props for all session info variables

--- shared/src/pages/SessionInfo/SessionInfo.jsx
+++ shared/src/pages/SessionInfo/SessionInfo.jsx
@@ -5,7 +5,7 @@ import Content from '../../components/Content'
 import PropTypes from 'prop-types'
 import './session-info.scss'
 
-function SessionInfo() {
+function SessionInfo({ name, email, phone, region, district, campus, sessionTitle, location}) {
     return (
         <Page>
             <TimeStamp />
@@ -13,14 +13,14 @@ function SessionInfo() {
                 <div className='qr-inner-content-wrapper'>
                     <h2 className='qr-inner-content-heading'>Session Information</h2>
                     <em>Review information below for accuracy.</em>
-                    <p className='session-info-label'><strong>Name: </strong>Bryan Wheeler</p>
-                    <p className='session-info-label'><strong>E-mail: </strong>bryan.wheeler@esc4.net</p>
-                    <p className='session-info-label'><strong>Phone: </strong>555-555-5555</p>
-                    <p className='session-info-label'><strong>Region: </strong>4</p>
-                    <p className='session-info-label'><strong>District: </strong>Spring Branch ISD</p>
-                    <p className='session-info-label'><strong>Campus: </strong>Stafford High School</p>
-                    <p className='session-info-label'><strong>Session Title: </strong>STEMulating Design Challenges in Science, Grades 9-12</p>
-                    <p className='session-info-label'><strong>Location: </strong>MCC102</p>
+                    <p className='session-info-label'><strong>Name: </strong>{name}</p>
+                    <p className='session-info-label'><strong>E-mail: </strong>{email}</p>
+                    <p className='session-info-label'><strong>Phone: </strong>{phone}</p>
+                    <p className='session-info-label'><strong>Region: </strong>{region}</p>
+                    <p className='session-info-label'><strong>District: </strong>{district}</p>
+                    <p className='session-info-label'><strong>Campus: </strong>{campus}</p>
+                    <p className='session-info-label'><strong>Session Title: </strong>{sessionTitle}</p>
+                    <p className='session-info-label'><strong>Location: </strong>{location}</p>
                     <div className='qr-button qr-button-center'>
                         <Link to="/print-badge">
                             <button>Print Badge</button>
@@ -30,6 +30,28 @@ function SessionInfo() {
             </Content>
         </Page>
       )
+}
+
+SessionInfo.defaultProps = {
+    name: 'First Last Name',
+    email: 'email@gmail.com',
+    phone: '555-555-5555',
+    region: '4',
+    district: 'Spring Branch ISD',
+    campus: 'Stafford High School',
+    sessionTitle: 'STEMulating Design Challenges in Science, Grades 9-12',
+    location: 'MCC102',
+}
+
+SessionInfo.propTypes = {
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired,
+    region: PropTypes.string.isRequired,
+    district: PropTypes.string.isRequired,
+    campus: PropTypes.string.isRequired,
+    sessionTitle: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
 }
 
 export default SessionInfo
