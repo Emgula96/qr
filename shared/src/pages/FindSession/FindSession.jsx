8b2a5e68a7d8f84b0050e9e14ad8dee7d686a8be
Bryan Wheeler <bwheel181@gmail.com>
2023-11-13 14:32:52 -0600

    Adds html and styling for session info data

--- shared/src/pages/FindSession/FindSession.jsx
+++ shared/src/pages/FindSession/FindSession.jsx
@@ -10,9 +10,9 @@ function FindSession() {
         <Page>
             <TimeStamp />
             <Content>
-                <div className='inner-content-wrapper'>
+                <div className='qr-inner-content-wrapper'>
                     <form>
-                        <h2 className='find-session-form-heading'>Search for Session</h2>
+                        <h2 className='qr-inner-content-heading'>Search for Session</h2>
                         <FormField label='E-mail Address' htmlFor='email-field' placeholder='Enter E-mail' type='email' note="We'll never share your email with anyone else." />
                         <FormField label='First Name' htmlFor='fname-field' placeholder='Enter First Name' type='text' />
                         <FormField label='Last Name' htmlFor='lname-field' placeholder='Enter Last Name' type='text' />
--- shared/src/pages/SessionInfo/SessionInfo.jsx
+++ shared/src/pages/SessionInfo/SessionInfo.jsx
@@ -9,8 +9,17 @@ function SessionInfo() {
         <Page>
             <TimeStamp />
             <Content>
-                <div className='inner-content-wrapper'>
-                    Session Info Goes Here
+                <div className='qr-inner-content-wrapper'>
+                    <h2 className='qr-inner-content-heading'>Session Information</h2>
+                    <em>Review information below for accuracy.</em>
+                    <p className='session-info-label'><strong>Name: </strong>Bryan Wheeler</p>
+                    <p className='session-info-label'><strong>E-mail: </strong>bryan.wheeler@esc4.net</p>
+                    <p className='session-info-label'><strong>Phone: </strong>555-555-5555</p>
+                    <p className='session-info-label'><strong>Region: </strong>4</p>
+                    <p className='session-info-label'><strong>District: </strong>Spring Branch ISD</p>
+                    <p className='session-info-label'><strong>Campus: </strong>Stafford High School</p>
+                    <p className='session-info-label'><strong>Session Title: </strong>STEMulating Design Challenges in Science, Grades 9-12</p>
+                    <p className='session-info-label'><strong>Location: </strong>MCC102</p>
                 </div>
             </Content>
         </Page>
