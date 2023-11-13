8799d19b541189e3c4a237ab9a90d402f85269b8
Bryan Wheeler <bwheel181@gmail.com>
2023-11-13 08:39:42 -0600

    Adds date portion of timestamp and styling

--- shared/src/components/TimeStamp/TimeStamp.jsx
+++ shared/src/components/TimeStamp/TimeStamp.jsx
@@ -1,16 +1,26 @@
+import { useState, useEffect } from 'react'
 import './timestamp.scss'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
+
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 function TimeStamp() {
+    const [date, setDate] = useState("")
+
+    // On mount, set up timers, get current date, etc.
+    useEffect(() => {
+        const today = new Date()
+
+        const dateString = `${weekday[today.getDay()]}, ${month[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`
+        setDate(dateString)
+    }, [])
+
     return (
-        <div>
-            TimeStamp Here
+        <div className='timestamp-wrapper'>
+            {date}
         </div>
       )
-}
-
-TimeStamp.propTypes = {
-    children: PropTypes.element.isRequired
 }
   
 export default TimeStamp
