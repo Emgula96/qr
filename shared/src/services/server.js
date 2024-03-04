21920a2c7d114fb6166a955c78237d7c3f09149f
Bryan Wheeler <bwheel181@gmail.com>
2024-03-04 09:48:55 -0600

    Adds proxy express app and ties into vite

--- shared/src/services/server.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-// TODO: The express proxy will handle API connections for both the attendees and nextgen backend APIs
-
-export const app = express();
-app.get('/api/test', (_, res) => 
-    res.json({ greeting: "Hello" }
-))
--- shared/vite.config.js
+++ shared/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import express from './express-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), express('src/server')],
   server: {
     watch: {
       usePolling: true,
