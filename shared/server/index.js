f87c71059a20498b3c0a98a161918a119ded39e1
Bryan Wheeler <bwheel181@gmail.com>
2024-03-04 16:22:47 -0600

    Adds front-end server, and local compose file

--- shared/server/index.js
+++ shared/server/index.js
@@ -1,30 +1,14 @@
-import express from 'express';
-const pgp = require('pg-promise')({
-    // Initialization Options
-});
+const express = require('express')
+const app = express()
+const port = 3030
 
+// const pgp = require('pg-promise')({});
+// const db = pgp('postgres://postgres:root@localhost:5432/postgres')
 
-const db = pgp('postgres://postgres:root@localhost:5432/postgres')
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-// TODO: The express proxy will handle API connections for both the attendees and nextgen backend APIs
-
-if (!process.env['VITE']) {
-  const frontendFiles = process.cwd() + '/dist'
-  app.use(express.static(frontendFiles))
-  app.get('/*', (_, res) => {
-    res.send(frontendFiles + '/index.html')
-  })
-  app.listen(process.env['PORT'])
-}
-
-export const app = express();
-
-app.get('/api/test', (_, res) => {
-    db.one('SELECT * FROM test.attendee')
-    .then((data) => {
-        console.log('DATA:', data.value)
-    })
-    .catch((error) => {
-        console.log('ERROR:', error)
-    })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
