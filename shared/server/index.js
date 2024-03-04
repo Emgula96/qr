335e7937c879d2c5521899a56827f785426ea6c7
Bryan Wheeler <bwheel181@gmail.com>
2024-03-04 10:23:01 -0600

    Cleanup

--- shared/server/index.js
+++ shared/server/index.js
@@ -1,4 +1,10 @@
 import express from 'express';
+const pgp = require('pg-promise')({
+    // Initialization Options
+});
+
+
+const db = pgp('postgres://postgres:root@localhost:5432/postgres')
 
 // TODO: The express proxy will handle API connections for both the attendees and nextgen backend APIs
 
@@ -12,6 +18,13 @@ if (!process.env['VITE']) {
 }
 
 export const app = express();
-app.get('/api/test', (_, res) => 
-  res.json({ greeting: "Hello" }
-))
+
+app.get('/api/test', (_, res) => {
+    db.one('SELECT * FROM test.attendee')
+    .then((data) => {
+        console.log('DATA:', data.value)
+    })
+    .catch((error) => {
+        console.log('ERROR:', error)
+    })
+})
