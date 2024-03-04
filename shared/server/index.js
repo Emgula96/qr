0677b82ae37ec59c21ba2d04bbffce7bf3cf9bb1
Bryan Wheeler <bwheel181@gmail.com>
2024-03-04 17:30:40 -0600

    Adds example server function

--- shared/server/index.js
+++ shared/server/index.js
@@ -1,12 +1,18 @@
 const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
+
 const app = express()
 const port = 3030
+
+app.use(cors())
 
 // const pgp = require('pg-promise')({});
 // const db = pgp('postgres://postgres:root@localhost:5432/postgres')
 
 app.get('/api/test', (req, res) => {
-  res.json({"data": "Region 4!"})
+  res.json({"data": "Welcome to Region 4!"})
 })
 
 app.listen(port, () => {
--- shared/src/pages/Kiosk/Kiosk.jsx
+++ shared/src/pages/Kiosk/Kiosk.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Page from '../../components/Page'
 import TimeStamp from '../../components/TimeStamp'
@@ -5,10 +6,21 @@ import Content from '../../components/Content'
 import './kiosk.scss'
 
 function Kiosk() {
+    const [header, setHeader] = useState()
+
+    useEffect(() => {
+        async function fetchData() {
+            const resp = await fetch(`${import.meta.env.VITE_FRONT_END_SERVER_URL}/api/test`)
+            const header = await resp.json()
+            setHeader(header.data)
+        }
+        fetchData()
+    }, [])
+
     return (
         <Page>
             <TimeStamp />
-            <Content>
+            <Content heading={header}>
                 <div className='kiosk-wrapper'>
                     <div className='kiosk-qr-image'>
                         <img src='https://www.esc4.net/mjsandbox/kiosk%20test/QR%20sample.jpg' />
