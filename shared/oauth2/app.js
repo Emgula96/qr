1582981fdb154c03b610842b278866ad748512ce
Bryan Wheeler <bwheel181@gmail.com>
2024-04-01 13:47:10 -0500

    Adds local redis client

--- shared/oauth2/app.js
+++ shared/oauth2/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { createClient } from 'redis'
 import getAccessToken from './token.js'
 
 dotenv.config()
@@ -8,10 +9,29 @@ dotenv.config()
 const app = express()
 app.use(cors())
 
+const client = await createClient({ url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}` })
+  .on('error', err => console.log('Redis Client Error', err))
+  .connect();
+
 app.get('/token', async (req, res) => {
   try {
-    const tokenResp = await getAccessToken()
-    return res.status(200).json({ data: tokenResp })
+    let token
+
+    if (client) {
+      token = await client.get('access-token')
+      if (!token) {
+        console.log("Fetching a fresh token")
+  
+        token = await getAccessToken()
+        await client.set('access-token', token, { EX: process.env.REDIS_ACCESS_TOKEN_TTL })
+      } else {
+        console.log("Using cached access token")
+      }
+    } else {
+      const token = await getAccessToken()
+    }
+
+    res.status(200).json({ data: token })
   } catch (err) {
     console.error("Error fetching the token", err)
     return res.status(500).send(err)
