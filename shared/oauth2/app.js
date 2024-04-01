6a8ef0ea6c9248c51acd4977954868b0b8b0abf8
Bryan Wheeler <bwheel181@gmail.com>
2024-04-01 14:26:04 -0500

    Adds some token server fixes

--- shared/oauth2/app.js
+++ shared/oauth2/app.js
@@ -9,9 +9,31 @@ dotenv.config()
 const app = express()
 app.use(cors())
 
-const client = await createClient({ url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}` })
+let url
+if (process.env.NODE_ENV === 'production') {
+  url = process.env.REDIS_HOST
+} else {
+  url = `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+}
+
+const client = await createClient({ url })
   .on('error', err => console.log('Redis Client Error', err))
   .connect();
+
+app.get('/health', (req, res) => {
+  const body = {
+    "isBase64Encoded": false,
+    "statusCode": 200,
+    "statusDescription": "200 OK",
+    "headers": {
+        "Set-cookie": "cookies",
+        "Content-Type": "application/json"
+    },
+    "body": "Hello from Lambda (optional)"
+  }
+
+  res.status(200).send(body)
+})
 
 app.get('/token', async (req, res) => {
   try {
