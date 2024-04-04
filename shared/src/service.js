ea906b27eb54a66f6cb8e94a4485cd483507c0bd
Bryan Wheeler <bwheel181@gmail.com>
2024-04-04 11:38:39 -0500

    Add new env variable

--- shared/src/service.js
+++ shared/src/service.js
@@ -1,6 +1,6 @@
 import rest from './util/rest.js' 
 
-const host = import.meta.env.MODE === 'development' ? 'http://localhost:3030/kiosk-express' : '/api'
+const host = import.meta.env.VITE_ENVIRONMENT === 'development' ? 'http://localhost:3030/kiosk-express' : '/api'
 
 async function getUserInfo() {
   return await rest.get(`${host}/v1/users`)
