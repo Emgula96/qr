0fa1512fb23e9daae94876a5e395cd0c31ba2bd5
Bryan Wheeler <bwheel181@gmail.com>
2024-04-17 11:48:44 -0500

    Adds schema changes to support payment

--- shared/db/scripts/seed_db.sql
+++ shared/db/scripts/seed_db.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS users_events;
 -- CreateTable Attendence
 CREATE TABLE attendees (
   "id" SERIAL PRIMARY KEY,
-  "user_id" TEXT NOT NULL UNIQUE,
+  "user_id" TEXT NOT NULL,
   "event_id" TEXT NOT NULL,
   "schedule_id" TEXT NOT NULL,
   "status" SMALLINT NOT NULL,
@@ -46,9 +46,10 @@ CREATE TABLE users (
 
 -- CreateTable Users_Events
 CREATE TABLE users_events(
-  user_id INTEGER REFERENCES users(id), 
-  event_id INTEGER REFERENCES events(id), 
-  CONSTRAINT users_events_pk PRIMARY KEY(user_id,event_id) 
+  "user_id" INTEGER REFERENCES users(id), 
+  "event_id" INTEGER REFERENCES events(id), 
+  CONSTRAINT "users_events_pk" PRIMARY KEY(user_id,event_id),
+  "paid" BOOL DEFAULT false
 );
 
 -- Seed
@@ -58,6 +59,6 @@ INSERT INTO users (first_name, last_name, email, phone) VALUES ('John', 'Doe', '
 INSERT INTO events (region, district, campus, title, location, start_time, room_number, presenter, facilitator, description, credits, notes, max_attendees) VALUES ('South Houston', 'District 9', 'Cool Campus', 'STEMulating Design Challenges in Science, Grades 9–12', 'Some Location', NOW(), 'MCC102', 'Jane Doe, PhD', 'Amanda Silva', 'Explore ways to implement TEKS-aligned design challenges in a high school science and CTE classroom. Experience using the engineering design process to address design challenges that support student growth and success in the classroom and beyond.', '6 CPE', 'This session has a restricted access window. Check in will not be permitted after 9:30 a.m.|This session requires check out in order to receive credit. Please scan your QR code once the session has ended', 30);
 INSERT INTO events (region, district, campus, title, location, start_time, room_number, presenter, facilitator, description, credits, notes, max_attendees) VALUES ('South Houston', 'District 9', 'Cool Campus', 'STEMulating Design Challenges in Science, Grades 9–12', 'Some Location', NOW(), 'MCC102', 'Jane Doe, PhD', 'Amanda Silva', 'Explore ways to implement TEKS-aligned design challenges in a high school science and CTE classroom. Experience using the engineering design process to address design challenges that support student growth and success in the classroom and beyond.', '6 CPE', 'This session has a restricted access window. Check in will not be permitted after 9:30 a.m.|This session requires check out in order to receive credit. Please scan your QR code once the session has ended', 30);
 
-INSERT INTO users_events (user_id, event_id) VALUES (1, 1);
-INSERT INTO users_events (user_id, event_id) VALUES (1, 2);
-INSERT INTO users_events (user_id, event_id) VALUES (2, 2);
+INSERT INTO users_events (user_id, event_id, paid) VALUES (1, 1, true);
+INSERT INTO users_events (user_id, event_id, paid) VALUES (1, 2, true);
+INSERT INTO users_events (user_id, event_id, paid) VALUES (2, 2, false);
