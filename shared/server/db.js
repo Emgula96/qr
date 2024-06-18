a44d12a3421f36fb6c4a7817c09f5512ae91d42b
ryansleon <160517806+ryansleon@users.noreply.github.com>
2024-06-18 09:56:51 -0500

    Revert "Initial queries"

--- shared/server/db.js
+++ shared/server/db.js
@@ -1,41 +1,21 @@
-import sql from 'mssql';
-import dotenv from 'dotenv';
-dotenv.config();
+import pg from 'pg'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const poolConfig = {
-  user: process.env.MSSQL_USER,
-  password: process.env.MSSQL_PASSWORD,
-  server: process.env.MSSQL_SERVER,
-  database: process.env.MSSQL_DATABASE,
-  port: parseInt(process.env.MSSQL_PORT, 10),
-  options: {
-    encrypt: false, // for Azure
-    trustServerCertificate: true, // to bypass self-signed certificate error
-  },
-};
-
-if (process.env.NODE_ENV === 'production') {
-  poolConfig.options.encrypt = true; // for Azure SQL Database
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  port: process.env.POSTGRES_PORT,
 }
 
-const poolPromise = new sql.ConnectionPool(poolConfig)
-  .connect()
-  .then(pool => {
-    console.log('Connected to MSSQL');
-    return pool;
-  })
-  .catch(err => {
-    console.error('Database Connection Failed!', err);
-    throw err;
-  });
+if (process.env.NODE_ENV === 'production') {
+  poolConfig.ssl = {
+    rejectUnauthorized: false
+  }
+}
 
-const query = async (text, params) => {
-  const pool = await poolPromise;
-  const request = pool.request();
-  params.forEach(param => {
-    request.input(param.name, param.type, param.value);
-  });
-  return request.query(text);
-};
+const pool = new pg.Pool(poolConfig)
 
-export { poolPromise, sql, query };
+export const query = (text, params) => pool.query(text, params)
