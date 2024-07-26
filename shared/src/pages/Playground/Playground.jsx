f94624a5f253561b0f37a7a62a27c658942e2316
ryansleon <ryan.leon@esc4.net>
2024-07-26 12:10:32 -0500

    Added playground page to test

---
+++ shared/src/pages/Playground/Playground.jsx
@@ -0,0 +1,77 @@
+/* eslint-disable indent */
+import { useState, useEffect } from 'react'
+import { useLocation, Link } from 'react-router-dom'
+import Page from '../../components/Page'
+import Content from '../../components/Content'
+import service from '../../service'
+import './playground.scss'
+import DeviceManager from './DeviceManager'; // Adjust the path as necessary
+
+const generateImageUrl = (deviceId, zoneId) => {
+    const bucketUrl = 'https://kiosk-maps.s3.us-east-2.amazonaws.com'
+    const kiosk = `k${deviceId.trim()}`
+    const sanitizedZoneId = zoneId.replace(/\s+/g, '')
+    return `${bucketUrl}/${kiosk}-${sanitizedZoneId}.png`
+}
+
+function Playground() {
+    const [qrSrc, setQrSrc] = useState('')
+    const [imageError, setImageError] = useState(false)
+    const location = useLocation()
+    // Get the query params
+    const queryParams = new URLSearchParams(location.search)
+    const userId = queryParams.get('userId')
+    const zoneId = queryParams.get('zoneId') // E.g., "MCC 103"
+    const deviceId = queryParams.get('deviceId') // E.g., "1" for Kiosk 1
+
+    useEffect(() => {
+        async function fetchData() {
+            const qrSrc = await service.generateQrCode(zoneId, userId)
+            setQrSrc(qrSrc)
+        }
+
+        fetchData()
+    }, [zoneId, userId])
+
+    const handleImageError = () => {
+        setImageError(true)
+    }
+
+    return (
+        <Page>
+            <Content>
+                <h2>DEMO QR CODE</h2>
+                <div className="playground-map-container">
+                    <img src={qrSrc} alt="QR Code" />
+                </div>
+                <div className="playground-map-container">
+                    {imageError ? (
+                        <p>
+                            Sorry, this map is currently unavailable. Please ask
+                            the front desk for location assistance.
+                        </p>
+                    ) : (
+                        <img
+                            src={generateImageUrl(deviceId, zoneId)}
+                            style={{ maxWidth: 500, maxHeight: 500 }}
+                            onError={handleImageError}
+                            alt={`Map to ${zoneId}`}
+                        />
+                    )}
+                </div>
+                <div className="qr-button qr-button-center">
+                    <Link
+                        to={{
+                            pathname: '/find-session',
+                            search: `deviceId=${deviceId}`,
+                        }}
+                    >
+                        <button>Find Session</button>
+                    </Link>
+                </div>
+            </Content>
+        </Page>
+    )
+}
+
+export default Playground
