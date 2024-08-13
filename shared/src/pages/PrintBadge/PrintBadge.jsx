fd1c34f53da22cd72e1998ea2c81edca6dec7610
jwfrasierreg4 <joseph.frasier@esc4.net>
2024-08-13 09:50:51 -0500

    KIOS-101 Print Badge Map Page
    Center welcome header and enlarge badge printing message
    
    - Added 'welcome-header' class to h1 to center "Welcome to Region 4!"
    - Created 'badge-printing-header' class for h2 to increase font size
    - Updated print-badge.scss with new styles for improved header presentation
    - Enhanced visual hierarchy and readability of PrintBadge component

--- shared/src/pages/PrintBadge/PrintBadge.jsx
+++ shared/src/pages/PrintBadge/PrintBadge.jsx
@@ -6,7 +6,7 @@ import Content from '../../components/Content';
 import service from '../../service';
 import './print-badge.scss';
 import Layout from '../FindSession/FindSessionButton/Layout';
-
+import Map from '../../assets/imgs/maps/1.png';
 const generateImageUrl = (deviceId = '1', zoneId = 'MCC 103') => {
   const bucketUrl = 'https://kiosk-maps.s3.us-east-2.amazonaws.com';
   const kiosk = `k${deviceId?.trim()}`;
@@ -40,16 +40,23 @@ function PrintBadge() {
   return (
     <Page>
       <Content>
-        <h2>DEMO QR CODE</h2>
-        <div className="print-badge-map-container">
+        <h1 className="welcome-header">Welcome to the Region 4</h1>
+        <h2 className="badge-printing-header">Your Badge Is Being Printed</h2>
+        <p className="badge-printing-paragraph">
+          Your Session is in MC 122{zoneId} See map below for your session
+          location.
+        </p>
+        {/* <div className="print-badge-map-container">
           <img src={qrSrc} alt="QR Code" />
-        </div>
+        </div> */}
         <div className="print-badge-map-container">
           {imageError ? (
-            <p>
-              Sorry, this map is currently unavailable. Please ask the front
-              desk for location assistance.
-            </p>
+            <img
+              className="print-badge-map-img"
+              src={Map}
+              onError={handleImageError}
+              alt={`Map to ${zoneId}`}
+            />
           ) : (
             <img
               src={generateImageUrl(deviceId, zoneId)}
@@ -59,7 +66,6 @@ function PrintBadge() {
             />
           )}
         </div>
-
         <div className="qr-button qr-button-left">
           <Layout deviceId={deviceId} />
         </div>
