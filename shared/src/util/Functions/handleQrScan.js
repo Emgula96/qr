25a6c584b379f363bc104dd66bac22a5a44e9674
Emgula96 <ethan_gula@yahoo.com>
2025-01-07 22:47:14 -0500

    function fix

--- shared/src/util/Functions/handleQrScan.js
+++ shared/src/util/Functions/handleQrScan.js
@@ -12,7 +12,7 @@ export const handleQrScan = async (decodedText, event, beepSound, setStatus, isL
   const { userId, sessionId } = parseScan(decodedText);
   const eventDateId = event?.event_dates[0]?.id;
   try {
-    const checkedIn = await service.checkIn(userId, sessionId, eventDateId);
+    const checkedIn = await service.checkInUser(userId, sessionId, eventDateId);
     
     if (checkedIn.error) {
       beepSound.play();
