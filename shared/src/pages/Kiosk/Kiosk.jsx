461a87d190eb270faa1fc99aef14b0059dca0a31
Emgula96 <ethan_gula@yahoo.com>
2025-09-18 13:09:55 -0400

    testing

--- shared/src/pages/Kiosk/Kiosk.jsx
+++ shared/src/pages/Kiosk/Kiosk.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 
 function Kiosk({ deviceId }) {
   const [errorState, setErrorState] = useState(null);
-
+  console.log('testing');
   const errorStates = {
     sessionNotFound: {
       title: 'Session Not Found',
